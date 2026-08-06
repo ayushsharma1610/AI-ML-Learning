99ba1182313579f53a1702a86430b08ab91b8216
ayushsharma1610 <ayussharma07@gmail.com>
2026-08-06 19:41:18 +0530

    Create Swapnum.py

---
+++ Swapnum.py
@@ -0,0 +1,13 @@
+num1=int(input("enter first number :"))
+
+num2=int(input("enter second number :"))
+
+num1= num1 + num2 
+
+num2 = num1 - num2
+num1 = num1 - num2
+
+print(" After swapping")
+
+print("First number:",num1)
+print("second number :",num2)
