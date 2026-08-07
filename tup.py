02a252302d64fe718d4ac1a30da788d9db94a45a
ayushsharma1610 <ayussharma07@gmail.com>
2026-08-07 16:54:50 +0530

    Create tup.py

---
+++ tup.py
@@ -0,0 +1,12 @@
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+print("Length:", len(numbers))
+
+print("Number of 30:", numbers.count(30))
+
+print("Position of 40:", numbers.index(40))
