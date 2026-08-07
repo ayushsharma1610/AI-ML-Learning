6fef7ca3750076de40e858f90cdaac9c8c014be7
ayushsharma1610 <ayussharma07@gmail.com>
2026-08-07 18:39:56 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,11 @@
+numbers = {10, 20, 30, 20, 40, 10}
+
+print("Set:", numbers)
+
+numbers.add(50)
+print("After adding 50:", numbers)
+
+numbers.remove(20)
+print("After removing 20:", numbers)
+
+print("Length of set:", len(numbers))
