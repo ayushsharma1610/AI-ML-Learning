e4fcbef5ae9159880ccfb23135def38c9b9ca9bb
ayushsharma1610 <ayussharma07@gmail.com>
2026-08-07 16:51:15 +0530

    Create lst.py

---
+++ lst.py
@@ -0,0 +1,16 @@
+number=[10,20,30,40,50]
+
+print("original list:",number)
+
+number.append(60)
+print("After adding 60:",number)
+
+number.remove(20)
+print("After removing 20:",number)
+
+print("First number:",number[0])
+print("last number:", number[-1])
+
+print("Length of list:", len(number))
+
+
